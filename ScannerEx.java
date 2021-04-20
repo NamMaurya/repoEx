34577695aed19c1dd00782c1aa48d8522abc73db
NamMaurya <mauryanamrata19@gmail.com>
2021-04-20 16:34:08 +0300

    merging

--- ScannerEx.java
+++ ScannerEx.java
@@ -1,8 +1,7 @@
 import java.util.*;
 public class ScannerEx{
-	static  Scanner sc = new Scanner(Sysytem.in);
+	static  Scanner sc = new Scanner(System.in);
 	public static void main(String args[]){
 		System.out.println("Which is your branch name?");
 	}
 	}
-

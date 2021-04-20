06f81da751093f143bff13a7481c5b7adf367aed
NamMaurya <78945102+NamMaurya@users.noreply.github.com>
2021-04-20 11:37:21 +0300

    change1

--- ScannerEx.java
+++ ScannerEx.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class ScannerEx{
 	static  Scanner sc = new Scanner(Sysytem.in);
 	public static void main(String args[]){
-		System.out.println("Which is your branch?");
+		System.out.println("Which is your branch name?");
 	}
 	}
 

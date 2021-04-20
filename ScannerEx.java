937693dedbd016c58453905220db4aa01c0c2c49
NamMaurya <mauryanamrata19@gmail.com>
2021-04-20 14:01:59 +0300

    Scanner Ex

---
+++ ScannerEx.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class ScannerEx{
+	static  Scanner sc = new Scanner(Sysytem.in);
+	public static void main(String args[]){
+		System.out.println("Which is your branch?");
+	}
+	}
+
